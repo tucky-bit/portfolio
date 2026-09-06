60b45155cdc310af554b0cd78a8f40ac8e54c580
Ken panizales <panizaleskj@gmail.com>
2026-09-06 09:00:26 +0800

    Add files via upload

---
+++ QR code/qwerty/QRCode.py
@@ -0,0 +1,15 @@
+import qrcode
+from PIL import Image, ImageDraw
+
+link_arr = []
+user= str(input("Enter the link:"))
+if user in link_arr:
+    print("Invalid Link")
+else:
+
+    qrcode = qrcode.QRCode(version=1, box_size=10, border=5)
+    qrcode.add_data(user)
+    qrcode.make(fit=True)
+
+    img = qrcode.make_image(fill_color="black", back_color="white")
+    img.show()
